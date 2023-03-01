671b1e41245eb1ba6c1a5a4c2622cf86bcc846e5
luocy <luocy@dustess.com>
2023-03-01 14:13:09 +0800

    feat: paas common

---
+++ common/mysql.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"github.com/sirupsen/logrus"
+	"go-micro.dev/v4/config"
+)
+
+/*
+{
+  "host": "127.0.0.1",
+  "user": "root",
+  "pwd": "123456",
+  "database": "micro",
+  "port": 3306
+}
+*/
+
+type MysqlConfig struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Pwd      string `json:"pwd"`
+	Database string `json:"database"`
+	Port     int64  `json:"port"`
+}
+
+// GetMysqlFromConsul 获取mysql配置
+func GetMysqlFromConsul(config config.Config, path ...string) *MysqlConfig {
+	mysqlConfig := &MysqlConfig{}
+	err := config.Get(path...).Scan(mysqlConfig)
+	if err != nil {
+		logrus.
+			WithField("keyword", "GetMysqlFromConsul: 获取数据库配置失败！").
+			Error(err)
+	}
+	return mysqlConfig
+}
